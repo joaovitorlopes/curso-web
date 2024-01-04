c9cbaba2ccf7c883b63da1d1ea919020a8f7613d
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-04 14:29:52 -0400

    template strings

---
+++ fundamentals/templateStrings.js
@@ -0,0 +1,10 @@
+// Template strings
+const nome = 'Rebeca'
+const concatenacao = `Olá ${nome}!`
+console.log(concatenacao)
+
+// expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase() // Arrow Function para uppercase
+console.log(`Ei... ${up('cuidado')}!`)
