3a737d6264bb106e90ec244f602c184e851cda2f
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-04 13:22:59 -0400

    types in JS

---
+++ fundamentals/typesJS.js
@@ -0,0 +1,13 @@
+// JavaScript é de tipagem fraca
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1416
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso
