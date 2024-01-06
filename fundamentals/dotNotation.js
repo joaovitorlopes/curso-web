e8f2ffbe04f2c151bd7a51c5803f978e39905e47
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-05 20:26:47 -0400

    dot notation in js

---
+++ fundamentals/dotNotation.js
@@ -0,0 +1,20 @@
+// Notação ponto é utilizada para chamar um atributo do objeto
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+// obj1['nome'] = 'Bola2'
+console.log(obj1.nome)
+
+function Obj(nome) {
+    this.nome = nome // deixando um atributo público
+    this.exec = function() {
+        console.log('Exec...')
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj3.exec()
